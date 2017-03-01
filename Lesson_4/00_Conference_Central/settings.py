46a2da93a41ff9ada347ef8937cb8b6579b21d13
navao <omarnavaalvarez@gmail.com>
2017-02-28 22:45:09 -0600

    Initial commit

---
+++ Lesson_4/00_Conference_Central/settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '993775420240-q4f9khpcdfmsi9pokec80f1vb37aotl8.apps.googleusercontent.com'
+
